85aa31bcbd5b55527eb5514dff990a4d6d37f0a1
SylvesterFraide <fridaysylvester2@gmail.com>
2025-08-03 15:35:09 +0300

    second commit

---
+++ src/Hooks/useAddTransaction.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../Config/firebase-config";
+import { useGetUserInfo } from "./useGetUserInfo";
+
+
+export const useAddTransaction = () => {
+  const { userID } = useGetUserInfo();
+  const transactionCollectionRef = collection(db, "transactions");
+  const addTransaction = async (
+    description,
+    transactionAmount,
+    transactionType
+  ) => {
+    await addDoc(transactionCollectionRef, {
+      userID,
+      description,
+      transactionAmount,
+      transactionType,
+      createAt: serverTimestamp(),
+    });
+  };
+
+  return <div>useAddTransaction</div>;
+};
---
+++ src/Hooks/useGetUserInfo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const useGetUserInfo = () => {
+  const { name, profilePhoto, userID, isAuth } = JSON.parse(
+    localStorage.getItem("auth")
+  );
+  
+  return { name, profilePhoto, userID, isAuth };
+};
